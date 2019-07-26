803fcaf7d177e0fe87abdcff663d26406a5e9a02
abdulsamadola <360campstore@gmail.com>
2019-07-26 02:41:49 +0100

    Message Alert Added to Account Header

--- src/Headers/AccountHeader.js
+++ src/Headers/AccountHeader.js
@@ -1,16 +1,37 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  TouchableWithoutFeedback
+} from "react-native";
 import colors from "../Theme/theme";
 
 export default class AccountHeader extends Component {
+  _alertIconRight = () => {
+    Alert.alert("Drawer will slide-out!");
+  };
+  _alertIconLeft = () => {
+    Alert.alert("I will change to search box!");
+  };
   render() {
     let { text, iconRight, iconLeft } = this.props;
+    const callHere = iconRight ? (
+      <TouchableWithoutFeedback onPress={this._alertIconRight}>
+        {iconRight}
+      </TouchableWithoutFeedback>
+    ) : null;
     return (
       <View style={styles.container}>
         <View style={styles.headerWrapper}>
-          <View style={styles.icon2}>{iconRight}</View>
+          <View style={styles.icon2}>{callHere}</View>
           <Text style={styles.headerText}>{text}</Text>
-          <View style={styles.icon}>{iconLeft}</View>
+          <View style={styles.icon}>
+            <TouchableWithoutFeedback onPress={this._alertIconLeft}>
+              {iconLeft}
+            </TouchableWithoutFeedback>
+          </View>
         </View>
       </View>
     );
