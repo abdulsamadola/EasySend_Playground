2e799b63b20f507a73b661be5c2fd5670be11e8d
abdulsamadola <360campstore@gmail.com>
2019-07-26 02:14:48 +0100

    Card Tab Completed

--- src/Containers/CardContainer.js
+++ src/Containers/CardContainer.js
@@ -1,16 +1,23 @@
 import React from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-  Alert
-} from "react-native";
+import { View, Alert, ActivityIndicator, StyleSheet, Text } from "react-native";
 import colors from "../Theme/theme";
 import { FontAwesome } from "@expo/vector-icons";
 
 export default class CardContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    };
+  }
+  Loading = () => {
+    Alert.alert("I have nothing to display!");
+  };
+  componentWillMount() {
+    setTimeout(() => {
+      this.Loading();
+    }, 3000);
+  }
   static navigationOptions = {
     tabBarLabel: "Card",
     tabBarOptions: {
@@ -27,9 +34,23 @@ export default class CardContainer extends React.Component {
   };
 
   render() {
-    return <View style={styles.container} />;
+    let content = !this.state.isLoading ? (
+      <View style={styles.container} />
+    ) : (
+      <View
+        style={[
+          styles.container,
+          { alignItems: "center", justifyContent: "center" }
+        ]}
+      >
+        <ActivityIndicator color={colors.blue} />
+        <Text style={{ color: colors.black }}>Please wait...</Text>
+      </View>
+    );
+    return <View style={styles.container}>{content}</View>;
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.primary,
